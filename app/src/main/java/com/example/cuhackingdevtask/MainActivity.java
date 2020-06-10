b09ac10e327772ca0cac0b3c5ffd5b118f57e479
Jtibs18 <jessicalovessoccer18@hotmail.com>
2020-06-10 12:25:54 -0400

    first version of a simple dev project that includes a button that increments the number on screen when clicked

---
+++ app/src/main/java/com/example/cuhackingdevtask/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.cuhackingdevtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button incrementButton;
+    TextView displayText;
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        incrementButton = (Button) findViewById(R.id.button);
+        displayText = (TextView) findViewById(R.id.textView);
+        displayText.setText(String.valueOf(counter));
+
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                displayText.setText(String.valueOf(counter));
+            }
+        });
+    }
+}
